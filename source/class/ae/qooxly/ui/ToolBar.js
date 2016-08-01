fc6dca6bef8936a1436b52f4c4182ee68c943eb3
adeliz <adelise.skope@gmail.com>
2016-08-01 11:08:08 +0200

    Binded hovermode with toolbar

--- source/class/ae/qooxly/ui/ToolBar.js
+++ source/class/ae/qooxly/ui/ToolBar.js
@@ -30,7 +30,7 @@ qx.Class.define("ae.qooxly.ui.ToolBar",{
         projectpart.add(saveProjectButton);
         
         //drag mode part
-        var dragmodePart = new qx.ui.toolbar.Part().set({enabled:true});
+        var dragmodePart = new qx.ui.toolbar.Part();
         this.add(dragmodePart);
         
         var panButton = new qx.ui.toolbar.RadioButton(this.tr("Pan"),"ae/qooxly/icons/move.png").set({
@@ -38,40 +38,14 @@ qx.Class.define("ae.qooxly.ui.ToolBar",{
         	show:"icon",
             toolTipText:this.tr("Pan")
         });
-        /*panButton.addListener("execute",function(e){
-        	qx.core.Init.getApplication().getChartView().relayout({"dragmode":"pan"});
-        	Plotly.Fx.init(qx.core.Init.getApplication().getChartView().getPlotlyDiv());
-        });*/
 
         var zoomButton = new qx.ui.toolbar.RadioButton(this.tr("Zoom"),"ae/qooxly/icons/zoom.png").set({
         	model:qx.data.marshal.Json.createModel("zoom"),
             show:"icon",
             toolTipText:this.tr("Zoom")
         });
-        /*zoomButton.addListener("execute",function(e){
-        	qx.core.Init.getApplication().getChartView().relayout({"dragmode":"zoom"});
-        	Plotly.Fx.init(qx.core.Init.getApplication().getChartView().getPlotlyDiv());
-        });*/
         
-        var selectBoxButton = new qx.ui.toolbar.RadioButton(this.tr("Box select"),"ae/qooxly/icons/select.png").set({
-            show:"icon",
-            toolTipText:this.tr("Box select")
-        });
-        selectBoxButton.addListener("execute",function(e){
-        	qx.core.Init.getApplication().getChartView().relayout({"dragmode":"select"});
-        	Plotly.Fx.init(qx.core.Init.getApplication().getChartView().getPlotlyDiv());
-        });
-        
-        var lassoBoxButton = new qx.ui.toolbar.RadioButton(this.tr("Lasso select"),"ae/qooxly/icons/lasso.png").set({
-            show:"icon",
-            toolTipText:this.tr("Lasso select")
-        });
-        lassoBoxButton.addListener("execute",function(e){
-        	qx.core.Init.getApplication().getChartView().relayout({"dragmode":"lasso"});
-        	Plotly.Fx.init(qx.core.Init.getApplication().getChartView().getPlotlyDiv());
-        });
-        
-        var radioGroup1 = new qx.ui.form.RadioGroup(panButton,zoomButton,selectBoxButton,lassoBoxButton);
+        var radioGroup1 = new qx.ui.form.RadioGroup(panButton,zoomButton);
         //radioGroup1.setAllowEmptySelection(true);
         //panButton.setValue(false);
         radioGroup1.setSelection([zoomButton]);
@@ -81,53 +55,40 @@ qx.Class.define("ae.qooxly.ui.ToolBar",{
         
         dragmodePart.add(panButton);
         dragmodePart.add(zoomButton);
-        /*dragmodePart.addSeparator();
-        dragmodePart.add(selectBoxButton);
-        dragmodePart.add(lassoBoxButton);*/
         
         //Hover mode part
-        var hovermodePart = new qx.ui.toolbar.Part().set({enabled:false});
+        var hovermodePart = new qx.ui.toolbar.Part();
         this.add(hovermodePart);
         
         var xButton = new qx.ui.toolbar.RadioButton(this.tr("X"),"ae/qooxly/icons/xlabel.png").set({
-            show:"icon",
+        	model:qx.data.marshal.Json.createModel("x"),
+        	show:"icon",
             toolTipText:this.tr("X")
-        });
-        xButton.addListener("execute",function(e){
-        	qx.core.Init.getApplication().getChartView().relayout({"hovermode":"x"});
-        	Plotly.Fx.init(qx.core.Init.getApplication().getChartView().getPlotlyDiv());
         });
         
         var yButton = new qx.ui.toolbar.RadioButton(this.tr("Y"),"ae/qooxly/icons/ylabel.png").set({
-            show:"icon",
+        	model:qx.data.marshal.Json.createModel("y"),
+        	show:"icon",
             toolTipText:this.tr("Y")
-        });
-        yButton.addListener("execute",function(e){
-        	qx.core.Init.getApplication().getChartView().relayout({"hovermode":"y"});
-        	Plotly.Fx.init(qx.core.Init.getApplication().getChartView().getPlotlyDiv());
         });
         
         var closestButton = new qx.ui.toolbar.RadioButton(this.tr("x"),"ae/qooxly/icons/label.png").set({
-            show:"icon",
+        	model:qx.data.marshal.Json.createModel("closest"),
+        	show:"icon",
             toolTipText:this.tr("Closest")
-        });
-        closestButton.addListener("execute",function(e){
-        	qx.core.Init.getApplication().getChartView().relayout({"hovermode":"closest"});
-        	Plotly.Fx.init(qx.core.Init.getApplication().getChartView().getPlotlyDiv());
         });
         
         var falseButton = new qx.ui.toolbar.RadioButton(this.tr("x"),"ae/qooxly/icons/nolabel.png").set({
-            show:"icon",
+        	model:qx.data.marshal.Json.createModel(false),
+        	show:"icon",
             toolTipText:this.tr("False")
-        });
-        falseButton.addListener("execute",function(e){
-        	qx.core.Init.getApplication().getChartView().relayout({"hovermode":false});
-        	Plotly.Fx.init(qx.core.Init.getApplication().getChartView().getPlotlyDiv());
         });
         
         var radioGroup2 = new qx.ui.form.RadioGroup(xButton,yButton,closestButton,falseButton);
         radioGroup2.setAllowEmptySelection(true);
-        xButton.setValue(false);
+        //radioGroup2.setSelection([closestButton]);
+        qx.core.Init.getApplication().getChartView().bind("model.layout.hovermode", radioGroup2, "modelSelection[0]");
+        radioGroup2.bind("modelSelection[0]", qx.core.Init.getApplication().getChartView(),"model.layout.hovermode" );
         
         hovermodePart.add(xButton);
         hovermodePart.add(yButton);
